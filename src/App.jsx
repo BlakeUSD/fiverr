c9354c5da876e71bedaec5c14d57614da8e39473
Dellano Blake <dellano.blake@outlook.com>
2023-06-18 23:57:34 -0500

    hotfix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import './App.css'
 import Login from "./components/Login"
-import Popup from "./components/Popup"
 import AccountPage from "./pages/AccountPage"
 import Landing from "./pages/Landing"
 
@@ -10,8 +9,8 @@ function App() {
     <>
       {/* <Popup /> */}
       {/* <Landing /> */}
-      {/* <Login /> */}
-      <AccountPage />
+      <Login />
+      {/* <AccountPage /> */}
     </>
   )
 }
